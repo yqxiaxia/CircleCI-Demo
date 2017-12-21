47dfbda6df1a2614763615de2a22652f4616c84e
yqxia <yqxia@thoughtworks.com>
2017-12-21 21:21:14 +0800

    [yuqing]make ci break

--- test/first_test.js
+++ test/first_test.js
@@ -4,6 +4,6 @@ let assert = require("assert");
 describe("test in project", () => {
   it("should return 2 when 1 add 1" , () => {
     let result = first.addNumber(1,1);
-    assert.equal(result,2);
+    assert.equal(result,3);
   })
 });
