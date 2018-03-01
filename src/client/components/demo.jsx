6c66975c4066c0e47fd890e2c92fca3ab3dbc789
yqxia <yqxia@thoughtworks.com>
2018-03-01 17:36:44 +0800

    [yuqing]add button click test

--- src/client/components/demo.jsx
+++ src/client/components/demo.jsx
@@ -2,12 +2,24 @@ import 'babel-polyfill';
 import React from 'react';
 
 export default class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowPara: true
+    };
+    this.changePara = this.changePara.bind(this);
+
+  }
   render() {
     return(
       <div>
         <h1>this is a title</h1>
-        <p>this is a paragraph</p>
+        <p style={{visibility: this.state.isShowPara ? 'visible': 'hidden'}}>this is a paragraph</p>
+        <button className="changePara" onClick={this.changePara}>change</button>
       </div>
     );
+  }
+  changePara() {
+    this.setState({isShowPara:!this.state.isShowPara});
   }
 }
--- test/client/demo.test.js
+++ test/client/demo.test.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, render, mount} from 'enzyme';
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
 import Demo from '../../src/client/components/demo.jsx';
 
 describe('test in frontend', () => {
-  it('call component mount', () => {
+  it('call component shallow', () => {
     const demo = shallow(<Demo/>);
-    expect(demo.html()).to.equal('<div><h1>this is a title</h1><p>this is a paragraph</p></div>');
+    expect(demo.find('h1').text()).to.equal('this is a title');
   });
+
+  it('call component render', () => {
+    const demo = render(<Demo/>);
+    expect(demo.find('h1').text()).to.equal('this is a title');
+  });
+
+  it('call component mount', () => {
+    const demo = mount(<Demo/>);
+    expect(demo.find('h1').text()).to.equal('this is a title');
+  });
+
+  it('call component button click', () => {
+    const demo = mount(<Demo/>);
+    expect(demo.find('p').prop('style').visibility).to.equal('visible');
+    demo.find('button .changePara').at(0).simulate('click');
+    expect(demo.find('p').prop('style').visibility).to.equal('hidden');
+  });
+
 });
